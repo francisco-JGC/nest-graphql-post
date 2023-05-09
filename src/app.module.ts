6cbb8e51f015289fd2e0509c01bd392c5f302b39
Francisco Garcia <jjjchico1@gmail.com>
2023-05-09 17:32:49 -0600

    feat: configure graphql configurations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,20 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { PostsModule } from './posts/posts.module';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { PostsModule } from './modules/posts/posts.module'
 
 @Module({
-  imports: [PostsModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+
+    PostsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
