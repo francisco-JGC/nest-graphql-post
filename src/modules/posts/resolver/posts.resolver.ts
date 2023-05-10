3081eb68d14f536769463ad61694bec9eb3737fc
Francisco Garcia <jjjchico1@gmail.com>
2023-05-09 21:46:16 -0600

    feat: new createPost mutation

--- src/modules/posts/resolver/posts.resolver.ts
+++ src/modules/posts/resolver/posts.resolver.ts
@@ -1,4 +1,5 @@
-import { Query, Resolver } from '@nestjs/graphql'
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql'
+import { CreatePostInput } from '../dto/create-post.input'
 import { Post } from '../entities/posts.entity'
 import { PostsService } from '../services/posts.service'
 
@@ -9,5 +10,10 @@ export class PostsResolver {
   @Query((returns) => [Post])
   posts() {
     return this.postsService.findAll()
+  }
+
+  @Mutation((returns) => Post)
+  createPost(@Args('postInput') postInput: CreatePostInput) {
+    return this.postsService.create(postInput)
   }
 }
--- src/modules/posts/services/posts.service.ts
+++ src/modules/posts/services/posts.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
+import { CreatePostInput } from '../dto/create-post.input'
 import { Post } from '../entities/posts.entity'
 @Injectable()
 export class PostsService {
@@ -8,5 +9,10 @@ export class PostsService {
 
   findAll(): Promise<Post[]> {
     return this.postRepo.find()
+  }
+
+  create(post: CreatePostInput): Promise<Post> {
+    const objectField = this.postRepo.create(post)
+    return this.postRepo.save(objectField)
   }
 }
